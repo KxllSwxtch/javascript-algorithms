b88455c65865061619dd1b082f0bdd700caacc75
Dmitriy Shin <dimashin9@gmail.com>
2018-12-19 10:25:03 -0500

    Removed leoNumbers, Modified mergeSort

--- mergeSort/index.js
+++ mergeSort/index.js
@@ -6,16 +6,27 @@
 
 */
 
-function mergeSort(array) {
-  if(array.length === 1) {
-    return array;
-  } else {
-    let C = [];
-    let A = Array.;
+const mergeSort = arr => {
+  if (arr.length < 2) return arr;
+
+  let middleIndex = Math.floor(arr.length / 2);
+  let FH = arr.slice(0, middleIndex); 
+  let SH = arr.slice(middleIndex, arr.length);
+
+  return merge(mergeSort(FH), mergeSort(SH));
+};
+
+const merge = (a, b) => {
+  let result = [];
+
+  while (a.length && b.length) {
+    let minElement = a[0] < b[0] ? a.shift() : b.shift();
+    result.push(minElement);
   }
-}
 
-// Helpers
-//////////
+  result = a.length ? result.concat(a) : result.concat(b);
 
-console.log(mergeSort([1500, 42, 1, 65, 988, 3548, 7785, 659, 454, 848484, 7454]));
+  return result;
+};
+
+console.log(mergeSort([1500, 42, 1, 65, 988, 3548, 7785, 659, 454, 848484, 7454])); // ​​​​​[ 1, 42, 65, 454, 659, 988, 1500, 3548, 7454, 7785, 848484 ]​​​​​
